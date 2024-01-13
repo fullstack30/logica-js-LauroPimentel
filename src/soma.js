bfe0d983069b357c0943bea407c9eb6862acf0bb
LauroPimentel <laurogabriel1133@gmail.com>
2024-01-13 15:53:13 -0300

    sla

--- src/soma.js
+++ src/soma.js
@@ -2,7 +2,7 @@ function soma(num1, num2) {
     const parsedNum1 = +num1;
     const parsedNum2 = +num2;
 
-    if (isNaN(parsedNum1) || isNaN(parsedNum2) || arguments.length !== 2) {
+    if (isNaN(parsedNum1) || isNaN(parsedNum2) || arguments.length !== 2 || typeof parsedNum1 !== 'number' || typeof parsedNum2 !== 'number') {
         return "Parâmetros inválidos!";
     }
 
