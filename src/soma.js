9075e028f045d6b8a0948e0da5406d5e5f26d498
LauroPimentel <laurogabriel1133@gmail.com>
2024-01-13 15:40:21 -0300

    Agora foi

--- src/soma.js
+++ src/soma.js
@@ -2,9 +2,11 @@ function soma(num1, num2) {
     const parsedNum1 = +num1;
     const parsedNum2 = +num2;
 
-    if(isNaN(parsedNum1) || isNaN(parsedNum2)) {
+    if (isNaN(parsedNum1) || isNaN(parsedNum2) || arguments.length < 2) {
         return "Parâmetros inválidos!";
     }
-    return parsedNum1 + parsedNum2
+
+    return parsedNum1 + parsedNum2;
 }
+
 module.exports = soma;
