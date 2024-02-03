259de657f013bdbe54f54929870d499edbe39a69
Lauro Pimentel <laurogabriel1133@gmail.com>
2024-02-03 10:45:42 -0300

    Jssss

--- soma_impar.js
+++ soma_impar.js
@@ -1,19 +1,26 @@
-
-
-function soma_impar(numero) {
+function somaImparesAteN(numero) {
     
-    if (!Number.isInteger(numero) || numero <= 0) {
-      throw new Error("Função aceita apenas números inteiros positivos.");
+    if (Number.isInteger(numero) && numero > 0) {
+      let somaImpares = 0;
+  
+      
+      for (let i = 1; i <= numero; i += 2) {
+        somaImpares += i;
+      }
+  
+      return somaImpares;
+    } else {
+      return "Função aceita apenas números inteiros positivos.";
     }
-
-    let soma = 0;
-
-    // Soma os números ímpares de 1 até o número fornecido
-    for (let i = 1; i <= numero; i += 2) {
-      soma += i;
-    }
-
-    return soma;
   }
-
-  module.exports = soma_impar;
+  
+  
+  const numeroInformado = 10;
+  const resultado = somaImparesAteN(numeroInformado);
+  
+  if (typeof resultado === 'number') {
+    console.log(`A soma dos números ímpares até ${numeroInformado} é: ${resultado}`);
+  } else {
+    console.log(resultado);
+  }
+  
