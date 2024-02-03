73dbe26814ee36a5e3b431241fda42e1179f4ed2
Lauro Pimentel <laurogabriel1133@gmail.com>
2024-02-03 10:26:22 -0300

    Soma impar js

---
+++ soma_impar.js
@@ -0,0 +1,20 @@
+// src/soma_impar.js
+
+function soma_impar(numero) {
+    // Verifica se o argumento é um número inteiro positivo
+    if (!Number.isInteger(numero) || numero <= 0) {
+      throw new Error("Função aceita apenas números inteiros positivos.");
+    }
+  
+    let soma = 0;
+  
+    // Soma os números ímpares de 1 até o número fornecido
+    for (let i = 1; i <= numero; i += 2) {
+      soma += i;
+    }
+  
+    return soma;
+  }
+  
+  module.exports = soma_impar;
+  
